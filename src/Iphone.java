3cd1c45cf564d170934a6f0738f7d812d460136e
Leandro de Souza Cruz Ferreira <leo_kickboxer@hotmail.com>
2023-09-22 17:27:54 -0300

    Projeto inicial

---
+++ src/Iphone.java
@@ -0,0 +1,61 @@
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    @Override
+    public void tocar() {
+        System.out.println("Tocando a música...");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Pausando a música...");
+    }
+
+    @Override
+    public void selecionarMusica() {
+        System.out.println("Selecionando a música...");
+    }
+
+    @Override
+    public void ligar() {
+        System.out.println("Ligando...");
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo...");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz...");
+    }
+
+    @Override
+    public void exibirPagina() {
+        System.out.println("Exibindo página...");
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando nova aba...");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizando a página...");
+    }
+
+    public static void main(String[] args) {
+        Iphone iphone = new Iphone();
+        iphone.tocar();
+        iphone.pausar();
+        iphone.selecionarMusica();
+        iphone.ligar();
+        iphone.atender();
+        iphone.iniciarCorreioVoz();
+        iphone.exibirPagina();
+        iphone.adicionarNovaAba();
+        iphone.atualizarPagina();
+    }
+    
+}
